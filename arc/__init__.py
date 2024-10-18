b013da801fd74e587e0a83ca50f983fd8b5c0dc6
Jose Gomez-Dans <jose.gomez-dans@kcl.ac.uk>
2024-10-18 11:28:45 +0100

    Explort the data reader too for covnenience.

--- arc/__init__.py
+++ arc/__init__.py
@@ -1,2 +1,3 @@
-from arc.arc_sample_generator import generate_arc_refs
-from arc.field_processor import arc_field
+from arc.arc_sample_generator import generate_arc_refs  # noqa
+from arc.field_processor import arc_field  # noqa
+from arc.s2_data_reader import get_s2_official_data  # noqa
